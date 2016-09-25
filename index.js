5bebe3cb1cc13a23171f053ba7bf517f862a8422
Rishi Masand <darthanakin44@gmail.com>
2016-09-24 20:05:26 -0400

    added dynamic routes for coins

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ app.get("/", function(req, res){
 	res.send("<h1>SpareChange</h1>");
 });
 
-app.get("/", function(req, res){
-	res.send("<h1>SpareChange</h1>");
+app.get("/change/:quarters/:dimes/:nickels/:pennies", function(req, res){
+	res.send("Spare change data delivered.");
+	var quarters = parseInt(req.params.quarters);
+	var dimes = parseInt(req.params.dimes);
+	var nickels = parseInt(req.params.nickels);
+	var pennies = parseInt(req.params.pennies);
+	console.log(quarters);
+	console.log(dimes);
+	console.log(nickels);
+	console.log(pennies);
 });
 
 http.listen(8080, function(){
